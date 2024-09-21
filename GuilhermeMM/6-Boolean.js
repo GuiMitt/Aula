a54e551ed213c91e2a56f5e122f314b9a7746e3d
Guilherme M Mittelmann <guilhermemittelmann11@gmail.com>
2024-09-20 21:22:16 -0300

    Add files via upload

---
+++ GuilhermeMM/6-Boolean.js
@@ -0,0 +1,47 @@
+/*
+console.log("Situações que retornam verdadeiro:");
+console.log('"String vazia":' , !!"");
+console.log('"String com varios espaços:", ', !!" ");
+console.log('String com varios espacos :',!! "   ");
+console.log('3;',!!3);
+console.log('-1:',!!-1)
+console.log('atencao à precedencia de operadores:');//é como na matematica
+console.log('!!5+5', !!5+5);
+console.log('5+5:',!!(5+5));
+console.log('5-5:',!! 5-5);//-4
+//acima retorna -4, pois primeiro converte !5, depois nega a negação, depois subtrai;
+
+console.log('5-5 retorna:',!!(5-5));//retorna false
+console.log(' : ',!!' ');
+console.log('{}',!!{});
+console.log('[]',!![]);
+console.log('infinity:',!!Infinity);//true. positivo infinity é um número muito grande.
+console.log('-infinity:',!!-Infinity);//true. infinity é um número muito pequeno.
+console.log('infinity - infinity:',!!(Infinity-Infinity));//NaN, false. infinity é um
+
+console.log('Situações qye retornam falso:');
+console.log('5-5:',!!(5-5));//retorna false
+console.log('5-5:',(!!5)-5);//retorna -4. primeiro gera 1, depois sybtrai 5.
+console.log('vazia:',!!"");
+console.log('vazia:',!!'');
+console.log('Nan:',!!NaN);
+console.log('underfined:',!!undefined);
+console.log('null:',!!null);
+*/
+/*
+//exercícios
+console.log('Exercícios');
+console.log('!7',!7);//false
+console.log('!!7',!!7);//true
+console.log('!!7-7',!!7-7);//-6
+console.log('!!(7-7)',!!(7-7));//false
+console.log('!(7-7)',!(7-7));//true
+*/
+console.log('Expressões lógicas');
+console.log('8>7:',(''||undefined||null(8>7)));//
+console.log('9',(''||undefined||null||9));//retorna
+console.log('sem !!', (''||undefined||null||'Oi'));//
+console.log('com !!',!!(''||undefined||null||'Oi'));//
+
+
+
